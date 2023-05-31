9edb3ff6a519eb2cc64eaff7673999f2b397af0c
ticccco <23436953+LucasPlacentino@users.noreply.github.com>
2023-05-31 14:42:26 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,10 @@ impl EventHandler for Handler {
     // Event handlers are dispatched through a threadpool, and so multiple
     // events can be dispatched simultaneously.
     async fn message(&self, ctx: Context, msg: Message) {
+        if msg.is_own(&ctx) {
+            return;
+        }
+        
         if msg.content == "Who asked?" {
             // Sending a message can fail, due to a network error, an
             // authentication error, or lack of permissions to post in the
